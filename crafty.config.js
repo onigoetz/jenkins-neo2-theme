00161a8dbc6dbf77da322880fb746095ae82f1a8
Stéphane Goetz <onigoetz@onigoetz.ch>
2019-02-07 23:49:36 +0100

    Base for new theme

---
+++ crafty.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    presets: [
+        "@swissquote/crafty-preset-postcss",
+        "@swissquote/crafty-runner-gulp"
+    ],
+    legacy_css: true,
+    css: {
+        "neo-light": {
+            source: "css/style.scss",
+            watch: ["css/**"]
+        }
+    },
+    postcss(crafty, config) {
+        config.processor("postcss-inline-svg")
+            .before("autoprefixer")
+            .setOptions({
+                path: __dirname + "/images"
+            });
+    }
+}
